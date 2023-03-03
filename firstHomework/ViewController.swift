c76361be0b583255063187b1a108f7453b5d628e
stopshien <stopforxcode@gmail.com>
2023-03-03 18:57:19 +0800

    第一份作業 僅需要觀看螢幕錄製功能，故給予簡單畫面變化功能，又因需上傳Git 部分，為求稍微多一點點東西可以看，將不使用Storyborard，僅使用程式碼部分建立Button 以及 Label，在按下右邊Button將會對背景顏色進行更換。

--- firstHomework/ViewController.swift
+++ firstHomework/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 取得螢幕的尺寸
+    let fullScreenSize = UIScreen.main.bounds.size
 
+    // 預設的按鈕樣式
+    var myButton = UIButton(type: .contactAdd)
+    
+    var myLabel = UILabel(frame: CGRect(x: 100, y: 300, width: 200, height: 100))
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        
+        myButton.center = CGPoint(
+          x: fullScreenSize.width * 0.4,
+          y: fullScreenSize.height * 0.2)
+        self.view.addSubview(myButton)
+
+        myButton = UIButton(type: .infoLight)
+        myButton.center = CGPoint(
+          x: fullScreenSize.width * 0.6,
+          y: fullScreenSize.height * 0.2)
+        self.view.addSubview(myButton)
+        
+        myLabel.text = "這是一個標籤"
+        myLabel.textColor = .white
+        myLabel.backgroundColor = .darkGray
+        myLabel.textAlignment = .center
+        self.view.addSubview(myLabel)
+
+        myButton.addTarget(
+            self,
+            action: #selector(ViewController.clickButton),
+            for: .touchUpInside)
+        
+        
     }
-
-
+    
+    @objc func clickButton() {
+        // 為基底的 self.view 的底色在黑色與白色兩者間切換
+        if self.view.backgroundColor!.isEqual(
+            UIColor.red) {
+            self.view.backgroundColor =
+            UIColor.blue
+        } else {
+            self.view.backgroundColor =
+                .red
+        }
+    }
+    
+   
+    
 }
-
