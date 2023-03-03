417403c05a3c52d82098f044293848f08aedb8f8
stopshien <stopforxcode@gmail.com>
2023-03-03 18:29:53 +0800

    Initial Commit

---
+++ firstHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstHomework
+//
+//  Created by 沈庭鋒 on 2023/3/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
